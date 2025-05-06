b9733bfe19edbcd67c0e3e32a40717bd94abf6b0
adityanagdekar <adityanagdekar@gmail.com>
2025-05-06 02:08:15 -0400

    Restructured repo to include both backend and frontend in monorepo & added frontend code

---
+++ blockcert-frontend/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [studentId, setStudentId] = useState("");
+  const [verificationResult, setVerificationResult] = useState("");
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async (e) => {
+    // if (!selectedFile) return;
+    // Placeholder logic
+    // setVerificationResult("✅ Certificate verified successfully!");
+
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+    formData.append("studentId", studentId);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/certificates/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setMessage(response.data);
+    } catch (error) {
+      console.error(error);
+      setMessage("Upload failed");
+    }
+  };
+
+  return (
+    <div className="Main-Container">
+      <div className="Header">
+        <h1>🎓 BlockCert: Academic Credential Verification</h1>
+      </div>
+      <div className="Content-Container">
+        <h2>Upload your certificate to verify authenticity</h2>
+
+        <div className="File-Container">
+          <div className="Upload-File-Container">
+            <div className="Student-Container">
+              <p>Enter Student Id</p>
+              <input
+                type="text"
+                value={studentId}
+                onChange={(e) => setStudentId(e.target.value)}
+                required
+              />
+            </div>
+            <br />
+            <p>Select or drag & drop your file</p>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload & Verify</button>
+          </div>
+
+          <div className="Verify-File-Container">
+            <p>Verification Result:</p>
+            <div className="Verification-Box">
+              {verificationResult || "No file uploaded yet."}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
